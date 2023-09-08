c71da166e0b9375413f5f5c3874929166e494094
kazimahmad22 <143375211+kazimahmad22@users.noreply.github.com>
2023-09-08 22:41:50 +0500

    Add files via upload

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -1,10 +1,50 @@
+const portfolioImg = [];
+
+// Create objects for the "graphic" category (17 images)
+for (let i = 1; i <= 17; i++) {
+  portfolioImg.push({
+    src: `./assets/graphic ${i}.webp`,
+    dataset: "graphic",
+  });
+}
+
+// Create objects for the "uiux" category (8 images)
+for (let i = 1; i <= 8; i++) {
+  portfolioImg.push({
+    src: `./assets/uiux ${i}.webp`,
+    dataset: "uiux",
+  });
+}
+
+// Create objects for the "photoshop" category (4 images)
+for (let i = 1; i <= 4; i++) {
+  portfolioImg.push({
+    src: `./assets/photoshop ${i}.webp`,
+    dataset: "photoshop",
+  });
+}
+
+const gallery = document.querySelector(".gallery");
+portfolioImg.forEach((data) => {
+  const html = `
+    <div  data-name="${data.dataset}">
+<a data-lightbox="models" data-title="" href="${data.src}">
+<img src="${data.src}" alt="" />
+</a>
+    </div>
+  `;
+
+  gallery.insertAdjacentHTML("beforeend", html);
+});
+
+// filter algorithm
+
 const filterButtons = document.querySelectorAll(".filterBtnContainer button");
 const filterableCards = document.querySelectorAll(".gallery div");
 const filterCards = (e) => {
   const activeFilter = document.querySelector(".active");
 
   activeFilter.classList.remove("active");
-
   e.target.classList.add("active");
 
   filterableCards.forEach((card) => {
@@ -22,3 +62,5 @@ const filterCards = (e) => {
 filterButtons.forEach((button) =>
   button.addEventListener("click", filterCards)
 );
+
+// animation
