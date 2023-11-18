f9b021c196a0ff2071fb350a2088caffd225fac8
kazim <kazimfaraztab@gmail.com>
2023-11-18 22:11:28 +0500

    projects added

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let mobMenuIcon = document.querySelector(".mobMenuIcon");
-let mobMenu = document.querySelector(".mobNavMenu");
+let mobMenuIcon = document.querySelector('.mobMenuIcon');
+let mobMenu = document.querySelector('.mobNavMenu');
 
-mobMenuIcon.addEventListener("click", function () {
-  if (mobMenu.style.display === "flex") {
-    mobMenu.style.display = "none";
+mobMenuIcon.addEventListener('click', function () {
+  if (mobMenu.style.display === 'flex') {
+    mobMenu.style.display = 'none';
   } else {
-    mobMenu.style.display = "flex";
+    mobMenu.style.display = 'flex';
   }
 });
